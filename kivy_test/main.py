c8ed4d98e9378b7a4db171631ea36472db218c7a
frbi-zvnt <francesco.bianchi.zvnt@outlook.com>
2020-10-09 13:45:57 +0200

    add kivy project

---
+++ kivy_test/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import kivy  
+  
+# It’s required that the base Class  
+# of your App inherits from the App class. 
+from kivy.app import App 
+from kivy.uix.gridlayout import GridLayout 
+  
+# This class stores the info of .kv file 
+# when it is called goes to my.kv file 
+class MainWidget(GridLayout):  
+    pass
+  
+# we are defining the Base Class of our Kivy App 
+class myApp(App): 
+    def build(self): 
+        # return a MainWidget() as a root widget 
+        return MainWidget() 
+  
+if __name__ == '__main__': 
+      
+    # Here the class MyApp is initialized 
+    # and its run() method called. 
+    myApp().run() 
+
+

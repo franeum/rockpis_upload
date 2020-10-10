da6b2f3fd7cd7af93c4becf51583a56fd56f5ff7
neum <neum@debian-top.myhome>
2020-10-10 20:11:31 +0200

    add colors to connections (quality)

--- connections/tastoma.py
+++ connections/tastoma.py
@@ -3,31 +3,39 @@
 import re 
 import subprocess 
 import pandas as pd 
-
-pattern = re.compile(r"(\*?\s+) (.+\s?) ([▂▄▆█_]+) ([0-9]+)")
-res = subprocess.check_output(["nmcli","-g","common","dev","wifi"])
-#res = res.decode('utf-8').split("\n")[1:-1]
-
-"""for i in res:
-    r = re.search(pattern, i)
-    if r:
-        print(r.groups()) """
-
-res = res.decode('utf-8')
-#print(pd.read_csv(res, sep='\t'))
-#print(res)
-
 from io import StringIO
-import csv
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+pd.set_option('display.width',None)
+pd.set_option('expand_frame_repr', False)
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+res = subprocess.check_output(["nmcli","-g","IN-USE,SSID,CHAN,RATE,SIGNAL,BARS,SECURITY","dev","wifi"])
+res = res.decode('utf-8')
 
 
 f = StringIO(res)
-#reader = csv.reader(f, delimiter=':')
-#for row in reader:
- #   print(row)
 prova = pd.read_csv(f, sep=':', header=None) #
-prova.columns = ['CONNECTED','SSID','MODE','CHAN','RATE','SIGNAL','BARS','SECURITY']
-#prova['CONNECTED'] = prova['CONNECTED'].map({'*':True})
-#print(prova.columns)
+prova.columns = ['CONNECTED','SSID','CHAN','RATE','SIGNAL','BARS','SECURITY']
+
 prova.index = prova.index + 1
-print(prova) 
+prova['levels'] = prova['BARS'].apply(lambda x: 4 - x.count('_'))
+prova['levels'] = prova['levels'].map({1: Fore.BLUE, 2: Fore.YELLOW, 3: Fore.GREEN, 4: Fore.GREEN})
+
+size = prova.shape[0]
+
+a_string = prova.iloc[:,:-1].to_string()
+splitted = a_string.split("\n")
+
+new = []
+
+for n,s in enumerate(splitted[1:]):
+    dim = len(str(n+1))
+    #print(prova['levels'][n+1])
+    new.append(s[:dim] + prova['levels'][n+1] + s[dim:] + Fore.RESET)
+
+print(splitted[0])
+print("\n".join(new))
